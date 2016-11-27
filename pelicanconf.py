790dd71d890aa12bc2b35f04409ed40c559b8d1b
Ilya V. Schurov <ilya@schurov.com>
2016-11-27 19:08:37 +0300

    new post

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Ilya V. Schurov'
-SITENAME = u'Math, Pics and Code'
+AUTHOR = u'Илья Щуров'
+SITENAME = u'Math, Pics and Code: блог Ильи Щурова'
 SITEURL = ''
 
 PATH = 'content'
@@ -23,12 +23,18 @@ AUTHOR_FEED_RSS = None
 GITHUB_URL = "https://github.com/ischurov"
 
 # Blogroll
-LINKS = tuple()
+LINKS = (('страница на сайте НИУ ВШЭ', 'http://hse.ru/staff/is'),
+        ('статьи на arXiv', 'https://arxiv.org/a/schurov_i_1.html'),
+        )
 
 # Social widget
 SOCIAL = (('twitter', 'https://twitter.com/ivoyager'),
           ('facebook', 'https://facebook.com/ilyaschurov'),
-          ('github', 'https://github.com/ischurov'),)
+          ('github', 'https://github.com/ischurov'),
+          ('stackoverflow',
+          'http://stackoverflow.com/users/3025981/ilya-v-schurov'),
+          ('flickr', 'http://flickr.com/photos/ivoyager')
+          )
 
 DEFAULT_PAGINATION = 10
 
@@ -39,12 +45,13 @@ MARKUP = ('md', 'ipynb')
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['ipynb.markup']
 THEME = "/Users/user/soft/pelican-sober-iv"
-# THEME = "/Users/user/soft/pelican-themes/pelican-sober"
+# THEME = "/Users/user/soft/pelican-themes/elegant"
 # RELATIVE_URLS = True
 #
-PELICAN_SOBER_ABOUT = """Меня зовут Илья Щуров. Математик. Любитель Py&shy;thon.
-Пишу научные статьи со смешными названиями. Работаю в НИУ ВШЭ. Понемногу
-программирую, фотографирую и путешествую."""
+PELICAN_SOBER_ABOUT = """Меня зовут Илья Щуров. Ма&shy;те&shy;ма&shy;тик. Лю&shy;би&shy;тель пи&shy;тон&shy;чи&shy;ков.
+Пишу на&shy;учные статьи со смешными названиями. Работаю в НИУ ВШЭ. Преподаю. Понемногу
+про&shy;грам&shy;ми&shy;рую, фото&shy;гра&shy;фи&shy;рую и пу&shy;те&shy;шес&shy;твую."""
 
 PELICAN_SOBER_TWITTER_CARD_CREATOR = "ivoyager"
-
+PELICAN_SOBER_STICKY_SIDEBAR = False
+IGNORE_FILES = ['.ipynb_checkpoints']
